eead66c0be770306610d2372c9be3a07422be930
carlprilo <prilochen@126.com>
2022-12-13 22:17:44 +0800

    test

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 window.onload = function() {
   document.getElementById('main-content').innerHTML = 'Hello, github pages :)'
+  var MyDiv = document.getElementById('main-content')
+  var button = document.createElement("input");
+  button.setAttribute("type", "button");
+  button.setAttribute("value", value);
+  button.setAttribute("id", id);
+  button.setAttribute("class", class);
+  button.style.width = "12%";
+  button.setAttribute("onclick", "function(this.id)");
+  MyDiv.appendChild(button);
 }
